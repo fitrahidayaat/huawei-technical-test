3aab878e413c6fa2e0a3f52f330df1308637b3ba
fitrahidayaat <fitrahidayaat@gmail.com>
2025-05-04 13:39:36 +0700

    [FIX][DATA] update data processing answer

--- data-processing/answer.sql
+++ data-processing/answer.sql
@@ -1,27 +1,27 @@
--- 1. Tambahkan satu personal dalam table dengan nama employee Albert
+-- 1. Add one person to the employees table with the name Albert
 INSERT INTO employees (Name, Position, Join_Date, Year_of_Experience, Salary)
-VALUES ('Albert', 'enginner', '2024-01-24', '2.5 Years', 50);
+VALUES ('Albert', 'enginner', '2024-01-24', 2.5, 50);
 
--- 2. Update table dengan posisi enginner with salaray $85
+-- 2. Update table with position engineer with salary $85
 UPDATE employees
 SET Salary = 85
-WHERE Position = 'enginner';
+WHERE Position = 'engineer';
 
--- 3. Hitung total pengeluaran salary saat tahun 2021
+-- 3. Calculate total salary expenditure for the year 2021
 SELECT SUM(Salary) AS Total_Salary_2021
 FROM employees
 WHERE (Join_Date <= '2021-12-31' AND (Release_Date IS NULL OR Release_Date >= '2021-01-01'))
 OR (Join_Date BETWEEN '2021-01-01' AND '2021-12-31')
 OR (Release_Date BETWEEN '2021-01-01' AND '2021-12-31');
 
--- 4. Sorting menampilkan 3 employee paling banyak yang memiliki Years of Experience
+-- 4. Sort and display the 3 employees with the most Years of Experience
 SELECT Name, Position, Year_of_Experience
 FROM employees
-ORDER BY CAST(SUBSTRING(Year_of_Experience, 1, LOCATE(' ', Year_of_Experience) - 1) AS DECIMAL) DESC
+ORDER BY Year_of_Experience DESC
 LIMIT 3;
 
--- 5. Tuliskan subquery untuk employee dengan posisi engginer yang memiliki exeperience kurang dari sama dengan 3 tahun
+-- 5. Write a subquery for employees with position engineer who have experience less than or equal to 3 years
 SELECT *
 FROM employees
 WHERE Position = 'enginner'
-AND CAST(SUBSTRING(Year_of_Experience, 1, LOCATE(' ', Year_of_Experience) - 1) AS DECIMAL) <= 3;
+AND Year_of_Experience <= 3;
